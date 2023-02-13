67e6094e495a9a370355d4587d5096e2dfd19a4f
tuan13 <Password1998#>
2023-02-13 15:00:45 +0700

    new item entity

---
+++ CiberSystem/Entities/Order.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CiberSystem.Entities
+{
+    public class Order
+    {
+        public int Id { get; set; }
+        public decimal Amount { get; set; }
+        public DateTime OverDate { get; set; }
+        public Customer Customer { get; set; }
+        public Product Product { get; set; }
+    }
+}
---
+++ CiberSystem/Entities/Product.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CiberSystem.Entities
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        public int Name { get; set; }
+        public decimal Price { get; set; }
+        public string Description { get; set; }
+        public int Quantity { get; set; }
+        public Category Category { get; set; }
+    }
+}
